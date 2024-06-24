cf7805df3c6f8c54cfae8ac99e109d1d689bd7f9
Bobby Plummer <tagtechinc@gmail.com>
2024-06-24 16:01:37 -0400

    Initial Commit

---
+++ MedTeam/MedTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedTeamApp.swift
+//  MedTeam
+//
+//  Created by Robert Plummer on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MedTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
